ebdca0ac40f362a5b9cc7f245d16a5645e60760b
Lorang <pierrick.lorang@gmail.com>
2023-12-11 10:40:22 -0500

    modified slightly Locked novelty

--- robosuite/environments/novelties/locked.py
+++ robosuite/environments/novelties/locked.py
@@ -7,6 +7,6 @@ class Locked(PickPlaceCan):
     """
 
     def __init__(self, **kwargs):
-        super().__init__(door_pos=(-0.05, 0.35, 0.1, -np.pi/2), plate_pos=(-0.4, -0.225, 0.1), door_locked=True, novelty="Door", **kwargs)
+        super().__init__(door_pos=(-0.05, 0.4, 0.1, -np.pi/2), plate_pos=(-0.4, -0.225, 0.1), door_locked=True, novelty="Door", **kwargs)
 
 
